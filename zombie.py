0f9e557c9ec9af9cec768ee6b3910f2d15df1956
tillmanjohnson <tillmanjohnson0109@gmail.com>
2026-02-11 02:10:04 +0000

    zombies created

--- zombie.py
+++ zombie.py
@@ -1,1 +1,12 @@
+import os
+import time
+
+pid = os.fork()
+if pid == 0:
+    print(f"Child ({os.getpid()}): started")
+    print(f"Child ({os.getpid()}) finished")
+else:
+    print(f"Parent ({os.getpid()}) started")
+    time.sleep(30)
+    print(f"Parent ({os.getpid()}) finished")
 
