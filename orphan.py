be423eebd790f85bdf262cda4f2598418f619540
tillmanjohnson <tillmanjohnson0109@gmail.com>
2026-02-11 02:00:08 +0000

    Created Orphan

--- orphan.py
+++ orphan.py
@@ -1,1 +1,11 @@
+import os
+import time
 
+pid = os.fork()
+if pid == 0:
+    print(f"Child ({os.getpid()}): Working for 30 seconds...")
+    time.sleep(30)
+    print(f"Child ({os.getpid()}) finished")
+else:
+    print(f"Parent ({os.getpid()})started")
+    print(f"Parent ({os.getpid()}) finished")
